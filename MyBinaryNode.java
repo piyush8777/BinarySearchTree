e9f8519bc4964229e85cba61a04134fc54b949bc
Piyush Shaw <piyushshaw8777@gmail.com>
2021-06-14 17:30:54 +0530

    Adding : Create a BST by adding 56 and then adding 30 & 70

---
+++ MyBinaryNode.java
@@ -0,0 +1,13 @@
+
+public class MyBinaryNode<k extends Comparable<k>> {
+    k key;
+    MyBinaryNode left;
+    MyBinaryNode right;
+
+    public MyBinaryNode(k key){
+        this.key = key;
+        this.left = null;
+        this.right = null;
+    }
+
+}
